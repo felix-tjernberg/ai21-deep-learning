71551e42137fd80d8ccddcaef32c50fce8a28551
Felix Tjernberg <felix.tjernberg@hotmail.se>
2022-05-12 17:05:05 +0200

    Complete 2c

--- lab/helper_functions.py
+++ lab/helper_functions.py
@@ -6,12 +6,18 @@ def flatten_list(list):
     return [item for sublist in list for item in sublist]
 
 
-def plot_sample_images(image_list, labels: bool = True):
+def plot_sample_images(image_list, labels: bool = True, labels_v2: bool = False):
     fig, axes = plt.subplots(2, 5, figsize=(10, 4))
     for index, ax in enumerate(axes.flatten()):
         if labels:
             ax.imshow(image_list[index][0], cmap="gray")
             ax.set_title(image_list[index][1])
+        elif labels_v2:
+            label = "cat"
+            if image_list[1][index] == 1:
+                label = "dog"
+            ax.imshow(image_list[0][index], cmap="gray")
+            ax.set_title(label)
         else:
             ax.imshow(image_list[index])
         ax.axis("off")
